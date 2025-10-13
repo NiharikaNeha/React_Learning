81b64b49784b762b86661db383867d214815c0f8
Niharika Swain <swainniharika27@gmail.com>
2025-10-13 20:46:54 +0530

    learned Props

---
+++ Props/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+
+const App = () => {
+
+    const users = [
+  {
+    "name": "Aarav Mehta",
+    "age": 28,
+    "city": "Mumbai",
+    "profession": "Software Engineer"
+  },
+  {
+    "name": "Neha Sharma",
+    "age": 25,
+    "city": "Delhi",
+    "profession": "UI/UX Designer"
+  },
+  {
+    "name": "Rohan Patel",
+    "age": 32,
+    "city": "Ahmedabad",
+    "profession": "Data Analyst"
+  },
+  {
+    "name": "Priya Singh",
+    "age": 29,
+    "city": "Bangalore",
+    "profession": "Digital Marketer"
+  },
+  {
+    "name": "Vikram Das",
+    "age": 35,
+    "city": "Kolkata",
+    "profession": "Project Manager"
+  }
+]
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
---
+++ Props/src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HeroSection = (props) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default HeroSection
